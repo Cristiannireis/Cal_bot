1864b47e6f48ee8b4aeb6253840bb9fd4ea0d177
Cristiane Reis <43080297+Cristiannireis@users.noreply.github.com>
2022-05-05 10:03:49 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -12,11 +12,11 @@ opção = 0
 while opção != 5:
     print("____"*20)
     print('''  Escolha uma opcção:
-    [ 1 ] somar
-    [ 2 ] multiplicar
-    [ 3 ] maior
-    [ 4 ] outros números
-    [ 5 ] sair do programa''')
+    [ 1 ] Somar
+    [ 2 ] Multiplicar
+    [ 3 ] Maior
+    [ 4 ] Você quer iniciar uma nova conta! digite um número e escolha a opção.
+    [ 5 ] Sair do programa''')
     opção = int(input('Qual é a sua opção? '))
     print("____"*20)
     if opção == 1:
@@ -32,12 +32,12 @@ while opção != 5:
             maior = n2
         print("Entre {} e {} o maior valor é {}".format(n1, n2, maior))
     elif opção == 4:
-        print("Informe os números novamente")
+        print("Informe um novo número")
         n1 = int(input("Primeiro valor: "))
         n2 = int(input("Segundo valor "))
     elif opção == 5:
         print("Finalizando ...")
     else:
         print("Opção inválida. Tente novamente")
-print("Fim do programa! Volte sempre")
+print("Fim do programa! Valeu")
 
